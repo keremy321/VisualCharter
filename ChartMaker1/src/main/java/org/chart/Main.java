7ab66979ac1108072ceb78863e2a736cee5faa5c
Kerem YILMAZ <163653782+keremy321@users.noreply.github.com>
2024-05-23 15:11:38 +0300

    Delete ChartMaker1 directory

--- ChartMaker1/src/main/java/org/chart/Main.java
+++
@@ -1,9 +0,0 @@
-package org.chart;
-
-import org.chart.gui.frames.MainFrame;
-
-public class Main {
-    public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
-    }
-}
